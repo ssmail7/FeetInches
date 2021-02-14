bec643769a1e77b74ac1375e31321c50a83505a7
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-13 21:26:23 -0500

    Add files via upload

---
+++ FeetInches.h
@@ -0,0 +1,56 @@
+//****************************************************************
+// Written by Steven Smail for COP3331
+//
+// FeetInches.h (Project 6)
+//
+// This is the class file that contains the FeetInches class;
+// including the Constructor, the Mutator functions, the Accessor
+// functions, and Overloaded Operator functions for < > << and >>.
+//****************************************************************
+
+#ifndef FEETINCHES_H#define FEETINCHES_H#include <iostream>using namespace std;// The FeetInches class holds distances or measurements // expressed in feet and inches.class FeetInches{private:  int feet;    // To hold a number of feet  int inches;  // To hold a number of inchespublic:  // Constructor  FeetInches(int f = 0, int i = 0)  {
+    feet = f;    inches = i;  }  // Mutator functions  void setFeet(int f)  { feet = f; }  void setInches(int i)  { inches = i; }  // Accessor functions  int getFeet() const  { return feet; }  int getInches() const  { return inches; }//**********************************************
+// Overloaded > operator
+//**********************************************  bool operator > (const FeetInches &right)
+  {
+    // Create boolean variable
+    bool value;
+
+    if(feet > right.feet)
+      value = true;
+    else if((feet == right.feet) && (inches > right.inches))
+      value = true;
+    else
+      value = false;
+
+    return value;
+  }//**********************************************
+// Overloaded < operator
+//**********************************************  bool operator < (const FeetInches &right)
+  {
+    // Create boolean variable
+    bool value;
+
+    if(feet < right.feet)
+      value = true;
+    else if((feet == right.feet) && (inches < right.inches))
+      value = true;
+    else
+      value = false;
+    return value;
+  }
+
+//**********************************************
+// Overloaded << operator
+//**********************************************  friend ostream& operator << (ostream& os, const FeetInches& fi)
+  {    os << fi.feet << " feet, " << fi.inches << " inches";    return os;
+  }
+
+//**********************************************
+// Overloaded >> operator
+//**********************************************  friend istream& operator >> (istream& is, FeetInches& fi)
+  {    char ch;
+    is >> fi.feet;   //get the feet    is >> ch;                   //read and discard the ','
+    is >> fi.inches; //get the inches
+    return is;
+  }};#endif
---
+++ TempDriver.cpp
@@ -0,0 +1,87 @@
+//***************************************************************
+// Written by Steven Smail for COP3331
+//
+// TempDriver.cpp (Project 6)
+//
+// This is the driver program that tests the FeetInches class
+// with function templates and operator overloads.
+//***************************************************************
+#include <iostream>
+#include <string>#include "FeetInches.h"using namespace std;template <class T>T minimum(T a, T b)
+{
+  if(a < b)
+    return a;
+  else
+    return b;
+}template <class T>T maximum(T a, T b)
+{
+  if(a > b)
+    return a;
+  else
+    return b;
+}
+int main(){
+  int firstInt, secondInt;
+  double firstDouble, secondDouble;
+  string firstString, secondString;
+  
+
+  // User Integer Input
+  cout << "Enter two integers: ";
+  cin >> firstInt >> secondInt;
+  // Minimum Integer Output
+  cout << "The minimum of " << firstInt
+       << " and " << secondInt << " is: "
+       << minimum(firstInt, secondInt) << endl;
+  // Maximum Integer Output
+  cout << "The maximum of " << firstInt
+       << " and " << secondInt << " is: "
+       << maximum(firstInt, secondInt) << endl;
+
+  // User Double Input
+  cout << "Enter two floating point numbers: ";
+  cin >> firstDouble >> secondDouble;
+  // Minimum Double Output
+  cout << "The minimum of " << firstDouble
+       << " and " << secondDouble << " is: "
+       << minimum(firstDouble, secondDouble) << endl;
+  // Maximum Double Output
+  cout << "The maximum of " << firstDouble
+       << " and " << secondDouble << " is: "
+       << maximum(firstDouble, secondDouble) << endl;
+
+  // To skip the remaining '\n' character
+  cin.ignore();
+
+  // User String Input
+  cout << "Enter the first string: ";
+  getline(cin, firstString);
+  cout << "Enter the second string: ";
+  getline(cin, secondString);
+  // Minimum String Output
+  cout << "The minimum of " << firstString
+       << " and " << secondString << " is: "
+       << minimum(firstString, secondString) << endl;
+  // Maximum String Output
+  cout << "The maximum of " << firstString
+       << " and " << secondString << " is: "
+       << maximum(firstString, secondString) << endl;
+
+  // Two FeetInches Objects
+  FeetInches distance1;
+  FeetInches distance2;
+
+  // User Object Input
+  cout << "Enter the first distance (in feet, inches format): ";
+  cin >> distance1;
+  cout << "Enter the second distance (in feet, inches format): ";
+  cin >> distance2;
+  // Minimum Object Output
+  cout << "The minimum of " << distance1
+       << " and " << distance2 << " is: "
+       << minimum(distance1, distance2) << endl;
+  // Maximum Object Output
+  cout << "The maximum of " << distance1
+       << " and " << distance2 << " is: "
+       << maximum(distance1, distance2) << endl;
+}
